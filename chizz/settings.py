808ccdfd3298fddff7ed5c3dfd2cac6e58cbf58e
MRJ-Joker-code <kingfemi090@gmail.com>
2026-09-09 17:02:33 +0100

    Fix database environment import

--- chizz/settings.py
+++ chizz/settings.py
@@ -11,7 +11,9 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
-
+from pathlib import Path
+import os
+import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
