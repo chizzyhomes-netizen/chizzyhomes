01fb9919478ebc239edd675280b3f82af6645f18
MRJ-Joker-code <kingfemi090@gmail.com>
2026-09-09 16:53:19 +0100

    Connect Django to Aiven PostgreSQL

--- chizz/settings.py
+++ chizz/settings.py
@@ -86,11 +86,12 @@ DEFAULT_FROM_EMAIL = 'ChizzyHomes <noreply@chizzyhomes.com>' # Name and address 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": dj_database_url.config(
+        default=os.environ.get("DATABASE_URL")
+    )
 }
 
 
